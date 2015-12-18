24e4b9714ed4536b7004bbeb6a9edec7aebbc1db
CalebSturges <sturg103@mail.chapman.edu>
2015-12-17 22:55:26 -0800

    Update julia.hpp

---
+++ CS510-Fall2015-FinalProject/Cpp-Mandelbrot/julia.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+
+using namespace std;
+
+complex<long double> juliamap(complex<long double> z, complex<long double> c);
+
+
